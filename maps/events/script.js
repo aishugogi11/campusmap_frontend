45582f719e85657c824503433c553838a863182e
Wi1ss0n <150631979+Wi1ss0n@users.noreply.github.com>
2026-03-07 20:58:13 -0800

    Added memory for side menu (if collapsed or not)

--- maps/events/script.js
+++ maps/events/script.js
@@ -1,13 +1,20 @@
 const toggle = document.querySelector(".menu-toggle");
 const sidemenu = document.querySelector(".sidemenu");
 
+let wasCollapsed = false;
+
 toggle.addEventListener("click", () => {
   sidemenu.classList.toggle("collapsed");
+  wasCollapsed = sidemenu.classList.contains("collapsed");
 });
 
 function handleResize() {
   if (window.innerWidth <= 752) {
     sidemenu.classList.remove("collapsed");
+  } else {
+    if (wasCollapsed) {
+      sidemenu.classList.add("collapsed"); 
+    }
   }
 }
 
